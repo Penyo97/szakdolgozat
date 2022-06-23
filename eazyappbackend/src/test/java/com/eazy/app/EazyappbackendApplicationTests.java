e88671e2f1542360b61477034b21c58faf69ba1e
Ádám Petrik <petrik97@stud.uni-obuda.hu>
2022-06-23 22:46:59 +0200

    first

---
+++ eazyappbackend/src/test/java/com/eazy/app/EazyappbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazy.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyappbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
