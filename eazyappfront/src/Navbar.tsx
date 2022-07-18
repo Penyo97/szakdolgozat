8a0bfda57d0c61697eb16bf39e5e0e88e2c024a0
Ádám Petrik <petrik97@stud.uni-obuda.hu>
2022-07-18 15:47:07 +0200

    fix left navbar

--- eazyappfront/src/Navbar.tsx
+++ eazyappfront/src/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {NavLink} from "react-router-dom";
+import {NavLink, useLocation} from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCartShopping, faBeer, faArrowLeftLong, faArrowRightLong} from "@fortawesome/free-solid-svg-icons"
 import './Navbar.css'
@@ -8,32 +8,45 @@ import logo from "./Assets/EZ_Logo.png"
 const Navbar = () => {
 
     const [hover, sethover] = useState<Boolean>(false);
+    const location = useLocation();
 
     let activeStyle = {
-        textDecoration:"none",
-        borderLeftColor:"white",
-        borderLeftStyle:"solid"
+        textDecoration: "none",
+        borderLeftColor: "white",
+        borderLeftStyle: "solid",
+        width: "90%"
     }
 
     let notactivestyle = {
-        textDecoration:"none",
+        textDecoration: "none",
+    }
+
+    let checker = (url:String): Boolean => {
+        return location.pathname === url;
     }
 
     return (
         <div className={hover ? "Navbar__ContainerBig" : "Navbar__Container"}>
+                        <span className="Navbar__LinkContainer"><img src={logo}
+                                                                     className={hover ? 'Navbar__logo Navbar__logohover' : 'Navbar__logo'}
+                                                                     alt="logo"/><span
+                            className={hover ? 'Navbar__MenuTextNone Navbar__MenuText' : 'Navbar__MenuTextNone'}>EAZY</span></span>
             <FontAwesomeIcon icon={!hover ? faArrowRightLong : faArrowLeftLong} onClick={() => sethover(!hover)}
                              className={hover ? "Navbar__arrow Navbar_arrowPadding" : "Navbar__arrow"}/>
 
-            <span className="Navbar__LinkContainer"><img src={logo}
-                                                         className={hover ? 'Navbar__logo Navbar__logohover' : 'Navbar__logo'} alt="logo"/><span
-                className={hover ? 'Navbar__MenuTextNone Navbar__MenuText' : 'Navbar__MenuTextNone'}>EAZY</span></span>
-            <NavLink to="/orders" style={({isActive})=> isActive ? activeStyle : notactivestyle}><span
+            <NavLink to="/orders" style={({isActive}) => isActive ? activeStyle : notactivestyle}><span
                 className="Navbar__LinkContainer"><FontAwesomeIcon icon={faCartShopping}
-                                                                   className={hover ? 'Navbar__Icons Navbar__IconPadding' : 'Navbar__Icons'}/><span
+                                                                   className={checker("/orders") && hover
+                                                                       ? 'Navbar__Icons Navbar__ActiveIconPadding'
+                                                                       : !checker("/orders") &&  hover ? 'Navbar__Icons Navbar__IconPadding'
+                                                                           : 'Navbar__Icons'}/><span
                 className={hover ? 'Navbar__MenuTextNone Navbar__MenuText' : 'Navbar__MenuTextNone'}>Orders</span></span></NavLink>
-            <NavLink to="/stock" style={({isActive})=> isActive ? activeStyle : notactivestyle}><span
+            <NavLink to="/stock" style={({isActive}) => isActive ? activeStyle : notactivestyle}><span
                 className="Navbar__LinkContainer"><FontAwesomeIcon icon={faBeer}
-                                                                   className={hover ? 'Navbar__Icons Navbar__IconPadding' : 'Navbar__Icons'}/><span
+                                                                   className={checker("/stock") && hover
+                                                                       ? 'Navbar__Icons Navbar__ActiveIconPadding'
+                                                                       : !checker("/stock") &&  hover ? 'Navbar__Icons Navbar__IconPadding'
+                                                                           : 'Navbar__Icons'}/><span
                 className={hover ? 'Navbar__MenuTextNone Navbar__MenuText' : 'Navbar__MenuTextNone'}>Stock</span></span></NavLink>
         </div>
     );
