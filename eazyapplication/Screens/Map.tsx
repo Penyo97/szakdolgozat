a7c5cf9242d3197b783f083f5ffbf52fc3580adf
Ádám Petrik <petrik97@stud.uni-obuda.hu>
2022-08-28 23:26:53 +0200

    make currentlocation to the map screen and make a calculator, what return zoom level

--- eazyapplication/Screens/Map.tsx
+++ eazyapplication/Screens/Map.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import {View, StyleSheet, TextInput, Dimensions,TouchableOpacity} from "react-native";
-import MapView,{Marker} from 'react-native-maps';
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, TextInput, Dimensions, TouchableOpacity, Text, Alert} from "react-native";
+import MapView, {Marker, Region} from 'react-native-maps';
 import {BottonNavbar} from "../Components";
 import {Icon} from "@rneui/base";
 import {COLORS} from "../AppAssets";
 import Animated from "react-native-reanimated";
+import * as Location from 'expo-location';
+//@ts-ignore
+import logo from "../assets/ez_logo.png";
 
-const TopNavbar = ({navigation}:any) => {
-    return(
+const TopNavbar = ({navigation}: any) => {
+    return (
         <View style={style.topnavBar}>
             <View style={style.inputContainer}>
-                <TextInput style={style.input} placeholder="Vendéglátóhelyek a környékeden." />
+                <TextInput style={style.input} placeholder="Vendéglátóhelyek a környékeden."/>
             </View>
-            <TouchableOpacity style={style.menu} onPress={()=>  navigation.openDrawer()}>
+            <TouchableOpacity style={style.menu} onPress={() => navigation.openDrawer()}>
                 <Icon name="bars" color="#fff" type="font-awesome-5" size={25}/>
             </TouchableOpacity>
         </View>
@@ -20,31 +23,63 @@ const TopNavbar = ({navigation}:any) => {
 }
 
 
+const Map = ({navigation}: any) => {
 
-const Map = ({navigation}:any) => {
+    const [location, setLocation] = useState(null);
+    const [zoomLevel, setZoomLevel] = useState<number>(0)
 
 
-        const region = {
-                latitude: 47.497913,
-                longitude: 19.040236,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
+    const zoomLevelCalculator = (region: Region) => {
+        setZoomLevel(Math.log2(360 * ((Dimensions.get("screen").width / 256) / region.longitudeDelta)) + 1);
+    }
+
+    useEffect(() => {
+        (async () => {
+            let {status} = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                return;
             }
+            let location = await Location.getCurrentPositionAsync({});
+            // @ts-ignore
+            setLocation(location);
+        })();
+    }, []);
 
-
+    const region = {
+        // @ts-ignore
+        latitude: location?.coords?.latitude,
+        //@ts-ignore
+        longitude: location?.coords?.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    }
 
 
     return (
         <Animated.View>
-            <TopNavbar  navigation={navigation}/>
-        <MapView
-            region={region} style={style.map}>
-            <Marker
-                coordinate={region}
-                title={"proba"}
-                description={"proba"}
-            />
-        </MapView>
+            <TopNavbar navigation={navigation}/>
+            <MapView
+                onRegionChange={region => zoomLevelCalculator(region)}
+                region={region} style={style.map}
+            >
+                {zoomLevel > 16 &&
+                    (
+                        <Marker
+
+                            coordinate={{
+                                latitude: 47.432035,
+                                longitude: 18.911758,
+                            }}
+                            title={"proba"}
+                            description={"proba"}
+                        >
+                            <View style={{backgroundColor: "red", padding: 10}}>
+                                <Text>SF</Text>
+                            </View>
+                        </Marker>
+                    )
+                }
+            </MapView>
             <BottonNavbar/>
         </Animated.View>
     );
@@ -54,24 +89,24 @@ export default Map;
 
 
 let style = StyleSheet.create({
-    map:{
-        height:Dimensions.get("screen").height
+    map: {
+        height: Dimensions.get("screen").height
     },
-    topnavBar:{
-        position:"absolute",
-        top:Dimensions.get("screen").height / 10,
-        zIndex:100,
-        flex:1,
-        alignSelf:"center",
-        flexDirection:"row"
+    topnavBar: {
+        position: "absolute",
+        top: Dimensions.get("screen").height / 10,
+        zIndex: 100,
+        flex: 1,
+        alignSelf: "center",
+        flexDirection: "row"
     },
-    menu:{
-        height:55,
-        width:55,
+    menu: {
+        height: 55,
+        width: 55,
         backgroundColor: COLORS.yellow,
-        justifyContent:"center",
-        borderRadius:25,
-        marginLeft:10
+        justifyContent: "center",
+        borderRadius: 25,
+        marginLeft: 10
     },
     inputContainer: {
         height: 55,
@@ -79,9 +114,9 @@ let style = StyleSheet.create({
         width: Dimensions.get("screen").width / 1.5,
         borderRadius: 10
     },
-    input:{
-        flex:1,
-        justifyContent:"center",
-        paddingLeft:10
+    input: {
+        flex: 1,
+        justifyContent: "center",
+        paddingLeft: 10
     },
-    })
+})
