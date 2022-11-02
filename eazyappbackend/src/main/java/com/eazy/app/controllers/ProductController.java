2bc95e242a29a9621a465cf8be6884703c90900c
Ádám Petrik <adam.petrik@webvalto.hu>
2022-11-02 16:13:10 +0100

    fixit ProductController

--- eazyappbackend/src/main/java/com/eazy/app/controllers/ProductController.java
+++ eazyappbackend/src/main/java/com/eazy/app/controllers/ProductController.java
@@ -5,14 +5,12 @@ import com.eazy.app.models.Product;
 import com.eazy.app.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import java.util.Set;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProductController {
 
     private final ProductService productService;
